9ed5f765f8cc43602d3439c2e841905bd2bfe461
CyborgVillager <almawijonathan@gmail.com>
2019-12-22 19:05:45 -0600

    v4-update

--- first_steps/ask_user_for_name.py
+++ first_steps/ask_user_for_name.py
@@ -1,12 +1,12 @@
 # This prgram asks the user for their name
 
-
 def ask_user():
     print('Hello user, ^_^ how may I call you? ')
     ask_user = input('Your Name: ')
     print('\n')
     print('Ah, thats a nice name ' + ask_user + ' has a nice ring tone to it!')
     print('A fun fact, your name length is ' + str(len(ask_user)) + ' ^_*')
+    print('\n')
 
 
     def ask_user_for_age():
--- first_steps/ask_user_indepth.py
+++ first_steps/ask_user_indepth.py
@@ -1,6 +1,6 @@
 import ask_user_for_name
 # this file is for
-# combination learning for importing an other python file,
+# combination learning for importing an other python file, while
 # input, nested functions, organizing code/etc
 
 def main():
