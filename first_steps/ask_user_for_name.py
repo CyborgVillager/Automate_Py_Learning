39a54a1cd09c4023e371b58fa6d46e0090c63a41
CyborgVillager <almawijonathan@gmail.com>
2019-12-22 18:00:06 -0600

    v0
    The Beginnin of learning to automate

---
+++ first_steps/ask_user_for_name.py
@@ -0,0 +1,7 @@
+# This prgram asks the user for their name
+
+print('Hello user, ^_^ how may I call you? ')
+ask_user = input('Your Name: ')
+
+print('Ah, thats a nice name ' + ask_user + ' has a nice ring tone to it!')
+print('A fun fact, your name length is ' + str(len(ask_user)) + ' ^_*')
