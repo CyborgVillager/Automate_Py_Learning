0e4bc44499d356594b4f1f7af6f9eefedb741441
CyborgVillager <almawijonathan@gmail.com>
2019-12-22 18:59:21 -0600

    v3 - update

--- first_steps/ask_user_for_name.py
+++ first_steps/ask_user_for_name.py
@@ -8,11 +8,23 @@ def ask_user():
     print('Ah, thats a nice name ' + ask_user + ' has a nice ring tone to it!')
     print('A fun fact, your name length is ' + str(len(ask_user)) + ' ^_*')
 
+
     def ask_user_for_age():
-        print('By the way ' + ask_user + ' how old are you? ')
-        ask_user_age = float(input('My age is: '))
-        print('Nice your age is: ' + str(round(ask_user_age)) + ' years old')
+        while True:
+            try:
+                print('By the way ' + ask_user + ' how old are you? ')
+                ask_user_age = float(input('My age is: '))
+                print('Nice your age is: ' + str(round(ask_user_age)) + ' years old')
+                age_add = ask_user_age + 1
+                print('You will be ' + str(round(age_add)) + ' in one year! ಠ‿↼')
+                break
+
+            except ValueError:
+                print('\n')
+                print('Sorry ' + ask_user + ' your input is not a valid , please try again! ^_^')
+
     ask_user_for_age()
+
 
 
 def spacing():
