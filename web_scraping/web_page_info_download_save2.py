1ccbcabc28c07d34f1b69ef760aed52b716d9c6b
CyborgVillager <almawijonathan@gmail.com>
2019-12-30 21:22:55 -0600

    v16
    Updated web_page_info_download_save2 to allow the user to type their own custom file name.
    
    After that it auto generates the file into a .txt file extension / notify the user that if the operation is a success or not.

--- web_scraping/web_page_info_download_save2.py
+++ web_scraping/web_page_info_download_save2.py
@@ -1,14 +1,17 @@
 import requests
 
-text_file_name = 'R&J.txt'
+user_file_name = input('Please enter a name for your file: ')
+file_extension_txt = '.txt'
+text_file = user_file_name + file_extension_txt
+
 try:
     aquire_info = requests.get('https://automatetheboringstuff.com/files/rj.txt')
     aquire_info.raise_for_status()
-    open_file = open('R&J.txt', 'wb')
+    open_file = open(text_file, 'wb')
     for chunk in aquire_info.iter_content(10000):
         open_file.write(chunk)
     open_file.close()
-    print('Success in aquiring info. To see the information please check ' + text_file_name)
+    print('Success in aquiring info. To see the information please check ' + text_file)
 except OSError:
     print('Failed to open the url, please check your connection to the url.')
 
