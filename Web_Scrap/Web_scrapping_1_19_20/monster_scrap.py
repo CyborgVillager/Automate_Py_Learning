c624efdeb1d4fb9fef21dccfc56daaba3287102f
CyborgVillager <almawijonathan@gmail.com>
2020-01-19 11:41:08 -0600

    v13 Check if Keyword is Refine
    Check if Keyword is Refine: Monster_Scrap.py will help assist the user if their keyword for their job/career is
                                        refined enough to be searched on the site. If not it will notify them of this issue
                                         and tell them to refine their keyword of either the job or location

--- Web_Scrap/Web_scrapping_1_19_20/monster_scrap.py
+++ Web_Scrap/Web_scrapping_1_19_20/monster_scrap.py
@@ -3,7 +3,7 @@ from colors import *
 
 # Ask's the user for some basic information on what job/career they are looking for & the location
 print('Once you have placed a Url, press Space then hit Enter to continue.')
-user_job_want = input('Type the job/career you are looking for: ')
+user_job_want = input('Type the job/career you are looking for: ').lower()
 user_job_location = input('Type the city name you would like this job/career to be: ' + red + '\n'
                           'If your looking for a remote position type remote: ' + end)
 print('\n')
@@ -23,10 +23,11 @@ job_elements = results.find_all('section', class_='card-content')
 
 for job_element in job_elements:
     '''  print(job_elements, end='\n'*2)
-
 # This shows the info regarding the jobs in a bit more in-depth info
 '''
     job_title_element = job_element.find('h2', class_='title')
+    # Further job filtering to ensure the user gets the job postings that they want to see
+    job_filter = results.find_all('h2', string=lambda text:user_job_want in text.lower())
     company_name_element = job_element.find('div', class_='company')
     location_element = job_element.find('div', class_='location')
 
@@ -40,6 +41,13 @@ for job_element in job_elements:
     print(blue + bold + str(location_element.text.strip()) + end)
     # Spaces between the other job postings
     print()
+
+if len(job_filter) == 0:
+    print('You have' + bold + red + ' no matches' + end + ', for your keyword: ' + bold + yellow +
+          str(user_job_want).capitalize() + end + '. Please refine your keyword/location.')
+else:
+    print('Total job matches for your keyword: ' + bold + yellow + str(user_job_want).capitalize() + end +  bold + red +
+      ' #' + str(len(job_filter)) + end)
 
 
 
